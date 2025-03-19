3cf48cff0cf282fd2f145fb21f4cc1df0640bf09
aman <amanm.ug23.cs@nitp.ac.in>
2025-03-19 17:04:52 +0530

    Error page added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
     <div className="relative z-10">
   <Navbar />
 </div>
-    <nav className="flex items-center justify-between bg-white/10 backdrop-blur-md p-1 shadow-md">
+    <nav className="flex items-center justify-between bg-white/10 backdrop-blur-md  shadow-md">
   <h1 className="text-green-800 font-bold text-xl flex justify-center items-center pl-80 "></h1>
   <button className="text-white font-semibold px-4 py-2 rounded-lg mt-6 pr-14">
     <Image src={chatbotIcon} alt="Chatbot Icon" width={50} height={50} />
--- src/components/NotFoundPage.tsx
+++ src/components/NotFoundPage.tsx
@@ -1,29 +1,31 @@
 import Image from "next/image"; 
 import errorimage from '../../public/image.png'
 import Link from "next/link";
+import { TbFaceIdError } from "react-icons/tb";
 
 const NotFoundPage = () => {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-black text-white">
-      <header className="w-full text-center py-6 font-bold text-xl">404 NOT FOUND</header>
+     <header className="w-full text-center py-6 font-bold text-8xl flex items-center justify-center gap-2">
+  404 NOT FOUND <i className="text-4xl text-red-500 pl-8 pt-2"><TbFaceIdError size={90} /></i>
+</header>
       <section className="flex flex-col md:flex-row items-center justify-center mt-20 px-6 md:px-16 lg:px-24">
           <div className="image flex justify-center md:w-1/2">
             <Image
               src={errorimage} 
-              alt="Scarecrow"
+              alt="signal"
               className="w-80 md:w-96 animate-pulse"
               width={384} 
               height={384} 
             />
           </div>
           <div className="text flex flex-col items-center md:items-start md:w-1/2 gap-6 text-center md:text-left">
-            <h1 className="text-3xl md:text-5xl font-bold">I have bad news for you</h1>
             <p className="text-lg md:text-xl w-3/4 md:w-2/3">
               The page you were looking for does not exist or is temporarily unavailable.
             </p>
             <Link
               href="/"
-              className="text-white bg-black px-6 py-3 rounded-md transition duration-300 hover:bg-white hover:text-black border border-black"
+              className="text-black bg-white px-6 py-3 rounded-md transition duration-300 hover:bg-black hover:text-white border border-black"
             >
               Back to Homepage
               </Link>
